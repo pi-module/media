334753d6fc1167eb2e924eeb97f8f170a63cd6ed
Frédéric TISSOT <contact@espritdev.fr>
2017-04-19 18:09:22 +0200

    https://github.com/pi-engine/guide/issues/1006 Cleanup mysql install script

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -1,22 +1,28 @@
 # Doc table
 CREATE TABLE `{doc}` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `path`         VARCHAR(255)        NOT NULL DEFAULT '',
-  `filename` varchar(255) NOT NULL DEFAULT '',
-  `attributes` text,
-  `mimetype` varchar(255) NOT NULL DEFAULT '',
-  `title` varchar(255) NOT NULL DEFAULT '',
-  `description` varchar(255) NOT NULL DEFAULT '',
-  `active` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
-  `time_created` int(10) UNSIGNED NOT NULL DEFAULT '0',
-  `time_updated` int(10) UNSIGNED NOT NULL DEFAULT '0',
-  `time_deleted` int(10) UNSIGNED NOT NULL DEFAULT '0',
-  `appkey` varchar(64) NOT NULL DEFAULT '',
-  `uid` int(10) UNSIGNED NOT NULL DEFAULT '0',
-  `count` int(10) NOT NULL DEFAULT '0'
+    `id` int(10) UNSIGNED NOT NULL,
+    `path` varchar(255) NOT NULL DEFAULT '',
+    `filename` varchar(255) NOT NULL DEFAULT '',
+    `attributes` text,
+    `mimetype` varchar(255) NOT NULL DEFAULT '',
+    `title` varchar(255) NOT NULL DEFAULT '',
+    `description` varchar(255) NOT NULL DEFAULT '',
+    `active` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
+    `time_created` int(10) UNSIGNED NOT NULL DEFAULT '0',
+    `time_updated` int(10) UNSIGNED NOT NULL DEFAULT '0',
+    `time_deleted` int(10) UNSIGNED NOT NULL DEFAULT '0',
+    `appkey` varchar(64) NOT NULL DEFAULT '',
+    `uid` int(10) UNSIGNED NOT NULL DEFAULT '0',
+    `count` int(10) NOT NULL DEFAULT '0',
+    `season` tinyint(4) DEFAULT NULL,
+    `updated_by` int(11) DEFAULT NULL,
+    `license_type` varchar(255) DEFAULT NULL,
+    `copyright` varchar(255) DEFAULT NULL,
+    `geoloc_latitude` float DEFAULT NULL,
+    `geoloc_longitude` float DEFAULT NULL,
+    `cropping` text,
+    `featured` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-ALTER TABLE `{doc}` ADD `season` TINYINT NULL AFTER `count`, ADD `updated_by` INT NULL AFTER `season`, ADD `license_type` VARCHAR(255) NULL AFTER `updated_by`, ADD `copyright` VARCHAR(255) NULL AFTER `license_type`, ADD `geoloc_latitude` FLOAT NULL AFTER `copyright`, ADD `geoloc_longitude` FLOAT NULL AFTER `geoloc_latitude`, ADD `cropping` TEXT NULL AFTER `geoloc_longitude`, ADD `featured` TINYINT NOT NULL AFTER `cropping`;
 
 ALTER TABLE `{doc}`
   ADD PRIMARY KEY (`id`),
@@ -25,17 +31,15 @@ ALTER TABLE `{doc}`
   ADD KEY `appkey` (`appkey`),
   ADD KEY `application` (`appkey`);
 
-ALTER TABLE `{doc}`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 # Test table
 CREATE TABLE `{test}` (
     `id` INT NOT NULL AUTO_INCREMENT ,
     `title` VARCHAR(255) NOT NULL ,
+    `main_image` INT NULL,
+    `additional_images` VARCHAR(255) NULL,
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
-
-ALTER TABLE `{test}` ADD `main_image` INT NULL AFTER `title`, ADD `additional_images` VARCHAR(255) NULL AFTER `main_image`;
 
 # Link table
 CREATE TABLE `{link}` (
