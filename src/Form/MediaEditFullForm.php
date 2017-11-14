0c7f59ee37212ce6bb5dfbb31eead08bee5e9648
Frédéric TISSOT <contact@espritdev.fr>
2017-11-14 12:00:44 +0100

    https://github.com/pi-engine/guide/issues/1020 Filename field before title on full media form (admin)

--- src/Form/MediaEditFullForm.php
+++ src/Form/MediaEditFullForm.php
@@ -24,17 +24,18 @@ class MediaEditFullForm extends MediaEditForm
      */
     public function init()
     {
-
-        parent::init();
-
-        $this->remove('submit');
-
         $this->add(array(
             'name'       => 'filename',
             'options'    => array(
                 'label'     => __('Filename'),
             ),
         ));
+
+        parent::init();
+
+        $this->remove('submit');
+
+
 
         $this->add(array(
             'name'       => 'uid',
