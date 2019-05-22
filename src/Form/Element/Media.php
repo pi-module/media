5493935b48f67c0b0f114dddd469afe77bb3d6d9
Frédéric TISSOT <contact@espritdev.fr>
2019-05-22 12:02:25 +0200

    Media modal XL

--- src/Form/Element/Media.php
+++ src/Form/Element/Media.php
@@ -162,7 +162,7 @@ class Media extends \Zend\Form\Element\Text
         $modalHtml = <<<HTML
         
 <div id="addMediaModal" class="modal fade" tabindex="-1" role="dialog">
-    <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-dialog modal-xl" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h4 class="modal-title">{$modalTitle} <span class="max"></span></h4>
