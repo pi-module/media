daa146af90fd5b299808a4e0a62711a2af4e07c3
voltan <djvoltan@gmail.com>
2019-11-29 14:41:19 +0330

    Update module version

--- config/module.php
+++ config/module.php
@@ -17,7 +17,7 @@ return [
     'meta'       => [
         'title'       => _a('Media'),
         'description' => _a('Module for media resources and APIs.'),
-        'version'     => '1.1.10',
+        'version'     => '1.2.0',
         'license'     => 'New BSD',
         'logo'        => 'image/logo.png',
         'readme'      => 'README.md',
